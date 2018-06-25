c98c80c6084f36b52ae1dcd045d04ee81674c4c7
brookew12 <brookiew2021@gmail.com>
2018-06-25 13:10:34 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,27 @@
 class Cat
-  # code goes here
+def initialize(cat, name="crookshanks", mood = "nervous")
+  @cat=cat
+  @name=name
+  @mood = mood
+end  # code goes here
+
+def cat
+@cat
 end
+
+def name
+  @name
+end
+
+def mood
+  @mood
+end
+
+def mood=(mood)
+  @mood = mood
+end
+end
+cat=Cat.new("cat")
+cat.mood = "playful"
+# puts cat.name
+puts cat.mood
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
-  # code goes here
+    def initialize(dog, name="Daisy", mood="nervous")
+      @dog=dog
+      @name=name
+      @mood=mood# code goes here
 end
+def dog
+  @dog
+end
+def name
+  @name
+end
+def mood
+  @mood
+end
+def mood=(mood)
+  @mood = mood
+end
+end
+
+dog=Dog.new("dog")
+dog.mood = "hungry"
+puts dog.name
+puts dog.mood
